0e3f3935734887b717cf1f91ddc81be2751b1c7a
Julio Furtado <juliocfurtado93@gmail.com>
2023-07-15 18:33:56 -0300

    Blog 100% funcional. Resta subir datos de produccion, y app de mensajeria interna

--- ProyectoFinal/YokoCino/forms.py
+++ ProyectoFinal/YokoCino/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 from YokoCino.models import *
 
-class formSetBlog(forms.Form):
-    title = forms.CharField(max_length=100)
-    subtitle = forms.CharField(max_length=100)
-    cuerpo = forms.CharField(max_length=1000)
-    author = forms.CharField(max_length=40)
-    fecha = forms.DateField()
-    imagen = forms.ImageField()
-
 class formSetPost(forms.ModelForm):
     imagen = forms.ImageField(required=False)
     class Meta:
         model = Post
         fields = ['titulo', 'slug', 'descripcion', 'contenido', 'imagen', 'autor', 'categoria', 'estado']
+
+class formSetCategoria(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nombre', 'estado']
+
+class formSetAutor(forms.ModelForm):
+    class Meta:
+        model = Autor
+        fields = ['nombre', 'apellido', 'link', 'email', 'estado']
--- ProyectoFinal/YokoCino/views.py
+++ ProyectoFinal/YokoCino/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render,redirect,get_object_or_404
 from YokoCino.models import *
 from Accounts.models import *
@@ -7,39 +7,33 @@ from YokoCino.forms import *
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-#from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import Group
 
 # Create your views here.
 
 def inicio(request):
     busqueda = request.GET.get('buscar')
+    staff_group = get_object_or_404(Group, name='Staff')
     if busqueda:
         posts = Post.objects.filter(
             Q(titulo__icontains = busqueda) |
             Q(descripcion__icontains = busqueda),
             estado = True,
         ).distinct()
+        pass
     else:
         posts = Post.objects.filter(estado = True)
+        pass
     paginator = Paginator(posts,5)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
-    return render(request,'YokoCino/inicio.html',{'posts':posts})
+    return render(request,'YokoCino/inicio.html',{'posts':posts,'staff_group': staff_group})
 
 def home(request):
     return render(request, "YokoCino/home.html")
 
 def about(request):
     return render(request, "YokoCino/about.html")
-
-#def inicio(request):
-#    avatar_url = getavatar(request)
-#    print (avatar_url)
-#    return render(request, "YokoCino/inicio.html", {"avatar_url":avatar_url})
-
-#def home(request):
-#    publicaciones = Blog.objects.all()
-#    return render(request, "YokoCino/home.html", {'publicaciones':publicaciones})
 
 def ensaladas(request):
     posts = Post.objects.filter(
@@ -77,10 +71,6 @@ def detallePost(request,slug):
     )
     return render(request,'YokoCino/post.html',{'detalle_post':post})
 
-#def blog(request):
-#    publicaciones = Blog.objects.all()
-#    return render(request, "YokoCino/blog.html", {'publicaciones':publicaciones})
-
 @login_required
 def setPost(request):
     if request.method == 'POST':
@@ -102,39 +92,42 @@ def setPost(request):
         miFormulario = formSetPost()
     return render(request, "YokoCino/setPost.html", {"miFormulario":miFormulario})
 
-#@login_required
-#def setEntry(request):
-#    if request.method == 'POST':
-#        miFormulario = formSetBlog(request.POST)
-#        print(miFormulario)
-#        if miFormulario.is_valid:
-#            data = miFormulario.cleaned_data
-#            entry = Blog(title=data["title"],subtitle=data["subtitle"],cuerpo=data["cuerpo"],author=data["author"],fecha=data["fecha"])    
-#            entry.save()
-#            return render(request,"YokoCino/home.html")    
-#    else:
-#        miFormulario = formSetBlog()
-#    return render(request, "YokoCino/setEntry.html", {"miFormulario":miFormulario})
-#
-#def getEntry(request):
-#    return render(request, "YokoCino/getEntry.html")
-#
-#def searchEntry(request):
-#    if request.GET["title"]:
-#        title = request.GET["title"]
-#        entries = Blog.objects.filter(title = title)
-#        return render(request, "YokoCino/getEntry.html", {"entries":entries})
-#    else:
-#        respuesta = "No se enviaron datos"
-#    return HttpResponse(respuesta)
+@login_required
+def setCategoria(request):
+    if request.method == 'POST':
+        miFormulario = formSetCategoria(request.POST)
+        if miFormulario.is_valid():
+            data = miFormulario.cleaned_data
+            categoria = Categoria(
+                nombre=data["nombre"],
+                estado=data["estado"])    
+            categoria.save()
+            return render(request,"YokoCino/inicio.html")
+    else:
+        miFormulario = formSetCategoria()
+    return render(request, "YokoCino/setCategoria.html", {"miFormulario":miFormulario})
 
-#def detalle_publicacion(request, pk):
-#    publicacion = get_object_or_404(Blog, pk=pk)
-#    return render(request, 'detalle_publicacion.html', {'publicacion': publicacion})
+@login_required
+def setAutor(request):
+    if request.method == 'POST':
+        miFormulario = formSetAutor(request.POST)
+        if miFormulario.is_valid():
+            data = miFormulario.cleaned_data
+            autor = Autor(
+                nombre=data["nombre"],
+                apellido=data["apellido"],
+                link=data["link"],
+                email=data["email"],
+                estado=data["estado"])    
+            autor.save()
+            return render(request,"YokoCino/inicio.html")
+    else:
+        miFormulario = formSetAutor()
+    return render(request, "YokoCino/setAutor.html", {"miFormulario":miFormulario})
 
+@login_required
 def editPost(request, slug):
     post = get_object_or_404(Post, slug=slug)
-
     if request.method == 'POST':
         miFormulario = formSetPost(request.POST, request.FILES, instance=post)
         if miFormulario.is_valid():
@@ -142,5 +135,4 @@ def editPost(request, slug):
             return redirect('detalle_post', slug=post.slug)
     else:
         miFormulario = formSetPost(instance=post)
-
     return render(request, 'YokoCino/editPost.html', {'miFormulario': miFormulario, 'post': post})
